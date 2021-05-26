17286d83e4d2edb240b4b0d646dbf7584b42f097
Elad Or <elad725@gmail.com>
2021-05-26 15:35:21 +0300

    Hebrew support in ActivityRecognizedService class

--- app/src/main/java/com/example/babyc/ActivityRecognizedService.java
+++ app/src/main/java/com/example/babyc/ActivityRecognizedService.java
@@ -4,6 +4,8 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -28,7 +30,9 @@ public class ActivityRecognizedService extends IntentService {
     public  ActivityRecognizedService(String name){ //another constructor
         super(name);
     }
-
+    //Shared prefs
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    SharedPreferences.Editor editor = prefs.edit();
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
@@ -41,6 +45,8 @@ public class ActivityRecognizedService extends IntentService {
 
 
     private void handleDetectedActivity(List<DetectedActivity> probableActivities){
+        if(prefs.getBoolean("English", true))
+        {
         for (DetectedActivity activity: probableActivities){
             switch(activity.getType()){
                 case DetectedActivity.IN_VEHICLE:{
@@ -107,9 +113,9 @@ public class ActivityRecognizedService extends IntentService {
                 {
                     Log.d(TAG, "handleDetectedActivity: UNKNOWN - " + activity.getConfidence());
                     Intent intent = new Intent("updateIntent");
-                    if(activity.getConfidence() >= 75) {
+                    if(activity.getConfidence() == 40) {
                         if (inCar) { inCar = false; }
-                        intent.putExtra("activityUpdate", "Current Activity:\n\nUnrecognizable");
+                        intent.putExtra("activityUpdate", "Current Activity:\n\nUnrecognized");
                         sendBroadcast(intent);
                     }
 
@@ -119,6 +125,88 @@ public class ActivityRecognizedService extends IntentService {
             }
         }
     }
+        else if (prefs.getBoolean("Hebrew", true))
+        {
+            for (DetectedActivity activity: probableActivities){
+                switch(activity.getType()){
+                    case DetectedActivity.IN_VEHICLE:{
+                        Log.d(TAG, "handDetectedActivity: IN_VEHICLE" + activity.getConfidence());
+                        if(activity.getConfidence() >= 75){
+                            //MainActivity.CurrentState.setText("Driving - In Car");
+                            //return "Driving - In Car";
+                            if (!inCar) { inCar = true; }
+                            Intent intent = new Intent("updateIntent");
+                            intent.putExtra("activityUpdate","פעילות נוכחית:\n\nברכב");
+                            sendBroadcast(intent);
+                        }
+
+
+                        break;
+                    }
+                    case DetectedActivity.ON_BICYCLE:{
+                        Log.d(TAG, "handDetectedActivity: ON_BICYCLE" + activity.getConfidence());
+                        if(activity.getConfidence() >= 75){
+                            if (inCar) { inCar = false; }
+                            Intent intent = new Intent("updateIntent");
+                            intent.putExtra("activityUpdate","פעילות נוכחית:\n\nאופניים");
+                            sendBroadcast(intent);
+                        }
+                    }break;
+                    case DetectedActivity.ON_FOOT:{
+                        Log.d(TAG, "handDetectedActivity: ON_FOOT" + activity.getConfidence());
+                        if(activity.getConfidence() >= 75){
+                            if (inCar) { inCar = false; }
+                            Intent intent = new Intent("updateIntent");
+                            intent.putExtra("activityUpdate","פעילות נוכחית:\n\nרגלית");
+                            sendBroadcast(intent);
+                        }
+                    }break;
+                    case DetectedActivity.RUNNING:{
+                        Log.d(TAG, "handDetectedActivity: RUNNING" + activity.getConfidence());
+                        if(activity.getConfidence() >= 75){
+                            if (inCar) { inCar = false; }
+                            Intent intent = new Intent("updateIntent");
+                            intent.putExtra("activityUpdate","פעילות נוכחית:\n\nריצה");
+                            sendBroadcast(intent);
+                        }
+                    }break;
+                    case DetectedActivity.STILL:{
+                        Log.d(TAG, "handDetectedActivity: STILL" + activity.getConfidence());
+                        if(activity.getConfidence() >= 75){
+                            if (inCar) { inCar = false; }
+                            Intent intent = new Intent("updateIntent");
+                            intent.putExtra("activityUpdate","פעילות נוכחית:\n\nדומם");
+                            sendBroadcast(intent);
+                        }
+                    }break;
+                    case DetectedActivity.WALKING:{
+                        Log.d(TAG, "handleDetectedActivity: WALKING" + activity.getConfidence());
+                        if(activity.getConfidence() >= 75){
+                            if (inCar) { inCar = false; }
+                            Intent intent = new Intent("updateIntent");
+                            intent.putExtra("activityUpdate","פעילות נוכחית:\n\nהליכה");
+                            sendBroadcast(intent);
+
+                        }
+                    }break;
+                    case DetectedActivity.UNKNOWN:
+                    {
+                        Log.d(TAG, "handleDetectedActivity: UNKNOWN - " + activity.getConfidence());
+                        Intent intent = new Intent("updateIntent");
+                        if(activity.getConfidence() == 40) {
+                            if (inCar) { inCar = false; }
+                            intent.putExtra("activityUpdate", "פעילות נוכחית:\n\nלא מזוהה");
+                            sendBroadcast(intent);
+                        }
+
+
+                    }break;
+
+                }
+            }
+        }
+    }
+
 }
 
 
