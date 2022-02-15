948d97db1e97d8bea32f25f3e5146266f93984f1
Elad Or <elad725@gmail.com>
2022-02-15 20:55:14 +0200

    Works well now, vibration bug fixed

--- app/src/main/java/com/example/babyc/MainActivity.java
+++ app/src/main/java/com/example/babyc/MainActivity.java
@@ -527,20 +527,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             //update current time variable
             curTime = System.currentTimeMillis();
 
-            //check if the state is driving (in this case - still, because it's for debug) and set alarm for 2 minutes after it
-            /*if( (CurrentState.getText().toString().equals("Still") || (CurrentState.getText().toString().equals("דומם")))  && !alarmSet && !dialogOnScreen){
+            //debug version of alarm set: check if the state is driving (in this case - still, because it's for debug) and set alarm for 2 minutes after it
+            if( (CurrentState.getText().toString().equals("Still") || (CurrentState.getText().toString().equals("דומם")))  && !alarmSet && !dialogOnScreen){
                 curTime2 = System.currentTimeMillis();
                 Log.d("Alarm", "Set");
                 Toast.makeText(contextOfApplication, "Alarm is Set", Toast.LENGTH_SHORT).show();
                 alarmSet = true;
             }
-*/
+
             //car version alarm set
-            if((CurrentState.getText().toString().equals("In Vehicle")||CurrentState.getText().toString().equals("בנסיעה")) && !alarmSet && !dialogOnScreen){
+            /*if((CurrentState.getText().toString().equals("In Vehicle")||CurrentState.getText().toString().equals("בנסיעה")) && !alarmSet && !dialogOnScreen){
                 curTime2 = System.currentTimeMillis();
                 Log.d("Alarm", "Set");
                 alarmSet = true;
-            }
+            }*/
 
 
             //car version - real version - using alarmDelaySeconds - activate only if state changed from ''in vehicle'' and 'x' time passed
@@ -583,10 +583,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 }
 
                 mp.start();
-                /*v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
                 long[] pattern = {0, 500};
-                v.vibrate(pattern, 5);
-                */
+                v.vibrate(pattern, 0);
+
                 dialogOnScreen = true; //boolean to make sure we don't have stacking dialogs
                 showDialog();
                 alarmSet = false;
